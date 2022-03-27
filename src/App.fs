6a74f6b76b3a709729d346f311f996cda417a0ab
plegerer <paul.legerer@gmx.at>
2022-03-27 21:31:08 +0200

    firstTest

--- src/App.fs
+++ src/App.fs
@@ -17,6 +17,39 @@ type Msg =
     | Decrement
     | OnPromiseSuccess of string * string
     | OnPromiseError of exn
+    | UpdateMsg
+
+
+let UpdateValue x =
+    Excel.run (fun context ->
+
+        let worksheet =
+            context.workbook.worksheets.getActiveWorksheet ()
+
+        let range = worksheet.getCell (1., 1.)
+
+        context
+            .sync()
+            .``then`` (fun _ ->
+                let customValue =
+                    ResizeArray [| ResizeArray [| Some(x) |] |]
+
+                range.values <- customValue
+
+                if Office.context.requirements.isSetSupported ("ExcelApi", "1.2") then
+                    range.format.autofitColumns ()))
+
+
+
+
+
+
+
+
+
+
+
+
 
 let init () =
     let initialCmd =
@@ -28,14 +61,19 @@ let init () =
 
 let update (msg: Msg) (state: State) =
     match msg with
-    | Increment -> { state with Count = state.Count + 3 }, Cmd.none
-    | Decrement -> { state with Count = state.Count - 1 }, Cmd.none
+    | Increment -> { state with Count = state.Count + 3 }, Cmd.ofMsg UpdateMsg
+    | Decrement -> { state with Count = state.Count - 1 }, Cmd.ofMsg UpdateMsg
     | OnPromiseSuccess (x, y) ->
         printfn "%A" x
         { state with Excelstate = x + " : " + y }, Cmd.none
     | OnPromiseError e ->
         printfn "%A" e
         state, Cmd.none
+    | UpdateMsg ->
+        let cmd =
+            Cmd.OfPromise.perform UpdateValue () (fun x -> ("neuer Wert", "Test") |> OnPromiseSuccess)
+
+        state, cmd
 
 let render (state: State) (dispatch: Msg -> unit) =
     Html.div [ Html.button [ prop.onClick (fun _ -> dispatch Increment)
